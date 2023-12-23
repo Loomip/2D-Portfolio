925ea80d29f32b53ec653e37b16f1927e74d49a7
Loomip <149350721+Loomip@users.noreply.github.com>
2023-12-23 18:07:08 +0900

    2D Jelly Village
    12월 23일 최종

--- Assets/Scripts/Jelly.cs
+++ Assets/Scripts/Jelly.cs
@@ -19,24 +19,38 @@ public class Jelly : MonoBehaviour
         yield return new WaitForSeconds(Random.Range(1f, 5f));
         animator.SetBool("isWalk", true);
         // 랜덤한 방향 결정
-        rendemVector = (Random.insideUnitCircle * 3).normalized;
+        rendemVector = (Random.insideUnitCircle).normalized;
         // 랜덤한 방향 일정 거리
-        float moveDistance = 1f;
+        float moveDistance = 3f;
         Vector2 targetPosition = (Vector2)transform.position + rendemVector * moveDistance;
+
+        if (!isMoving)
+        {
+            animator.SetBool("isWalk", false);
+            yield break; // 코루틴 종료
+        }
 
         while ((Vector2)transform.position != targetPosition)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime);
             yield return null;
         }
-        yield return new WaitForSeconds(1f);
         animator.SetBool("isWalk", false);
         isMoving = false;
     }
-    //2. 자동 랜덤 위치 이동
     //3. 자동으로 올라가는 경험치
     //4. 최대 렙과 각 렙에 올라가는 경험치
     //5. 젤리 끼리 닿으면 스쳐지나가지 말고 반대방향으로 (벽에 닿으면 다른 위치로)
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Jelly"))
+        {
+            StopCoroutine(RandomMove());
+            isMoving = false;
+        }
+    }
+
     //6. 터치 후 렙에 따른 돈 생성 및 수확
 
     private void Awake()
